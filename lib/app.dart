74ea930ff78c3f87f525dc83ac6e9e91ef3b1c67
Bruno Mazzocchi <brunomazzocchidev@gmail.com>
2023-10-15 21:40:04 -0600

    Initial commit

---
+++ lib/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+/// This is the main application widget.
+class App extends StatelessWidget {
+  /// The [key] parameter is required for the [StatelessWidget] to be able to
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_app_practice/app.dart';
+
+void main () {
+  return runApp(
+    const App(),
+  );
+}
